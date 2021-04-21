a83ac9bd2224f1bdeea7018b7259e2eb3419a0a7
pedroesmerio <p.h.esmerio@gmail.com>
2021-04-21 12:12:52 -0300

    Build feita com SSG (estático)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,28 +16,6 @@
 //}
 
 //-------------------SSR---------------------
-export default function Home(props) {
-  console.log(props.episodes);
-
-  return (
-    <>
-      <h1>Simple Home</h1>
-      <p>{JSON.stringify(props.episodes)}</p>
-    </>
-  );
-}
-
-export async function getServerSideProps() {
-  const res = await fetch('http://localhost:3333/episodes');
-  const data = await res.json();
-  return {
-    props: {
-      episodes: data,
-    },
-  };
-}
-
-//-------------------SSG---------------------
 //export default function Home(props) {
 //console.log(props.episodes);
 
@@ -49,13 +27,35 @@ export async function getServerSideProps() {
 //);
 //}
 
-//export async function getStaticProps() {
+//export async function getServerSideProps() {
 //const res = await fetch('http://localhost:3333/episodes');
 //const data = await res.json();
 //return {
 //props: {
 //episodes: data,
 //},
-//revalidate: 60 * 60 * 8,
 //};
 //}
+
+//-------------------SSG---------------------
+export default function Home(props) {
+  console.log(props.episodes);
+
+  return (
+    <>
+      <h1>Simple Home</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:3333/episodes');
+  const data = await res.json();
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
+}
