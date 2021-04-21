1773da45a21bea4770078f539a2b87d800e2a174
pedroesmerio <p.h.esmerio@gmail.com>
2021-04-21 12:11:30 -0300

    Build feita com SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,61 @@
-import { useEffect } from 'react';
+//-------------------SPA---------------------
+//import { useEffect } from 'react';
 
-export default function Home() {
-  useEffect(() => {
-    fetch('http://localhost:3333/episodes')
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  }, []);
+//export default function Home() {
+//useEffect(() => {
+//fetch('http://localhost:3333/episodes')
+//.then((res) => res.json())
+//.then((data) => console.log(data));
+//}, []);
+
+//return (
+//<>
+//<h1>Simple Home</h1>
+//</>
+//);
+//}
+
+//-------------------SSR---------------------
+export default function Home(props) {
+  console.log(props.episodes);
 
   return (
     <>
       <h1>Simple Home</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
     </>
   );
 }
+
+export async function getServerSideProps() {
+  const res = await fetch('http://localhost:3333/episodes');
+  const data = await res.json();
+  return {
+    props: {
+      episodes: data,
+    },
+  };
+}
+
+//-------------------SSG---------------------
+//export default function Home(props) {
+//console.log(props.episodes);
+
+//return (
+//<>
+//<h1>Simple Home</h1>
+//<p>{JSON.stringify(props.episodes)}</p>
+//</>
+//);
+//}
+
+//export async function getStaticProps() {
+//const res = await fetch('http://localhost:3333/episodes');
+//const data = await res.json();
+//return {
+//props: {
+//episodes: data,
+//},
+//revalidate: 60 * 60 * 8,
+//};
+//}
